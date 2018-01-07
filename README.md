adebe56a540e411db4b83f31ca4ffa5d7d9dc7b3
ryanmwright <ryan@ryanmwright.com>
2018-01-07 15:48:08 -0500

    add missing file and readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # generator-web-browser-extension
+
+This project scaffolds a browser extension that's compatible with browsers that utilize the WebExtensions API (i.e. Chrome/Firefox/Edge). It contains:
+
+- The base project
+- Webpack with hot reload that reloads the extension in the browser automatically
+- A build pipeline to build and package your app
+
+Right now, only a base extension with Angular for the browser action and options page is supported.
+
+# Getting started
+
+Run:
+
+```
+yo generator-web-browser-extension
+```
+
+this will scaffold your project and run an NPM install.
+
+Then cd into your application's folder and run:
+
+```
+npm run serve
+```
+
+This will create a 'development' folder under 'build-process/dist'.
+
+In Chrome go to chrome://extensions/ and drag the 'development' folder onto the extensions list.
+
+That's it! You can now start writing code. Changes you make will trigger the browser to reload the extension automatically.
+
+# Angular
+
+Angular and the Angular CLI is used for the action and options page under the (actions and options folders in src/). Each one is a separate application under the .angular-cli.json config file. The building of them is automated in Gulp.
+
+# Build Pipeline
+
+To package your application for production run:
+
+```
+npm run package:prod
+```
+
+This will create a 'package' folder under 'build-process/dist/production'. That folder contains a zip folder with your packaged app that can be uploaded to the store.
---
+++ generators/angular-cli/templates/build-process/build.angular.js
@@ -0,0 +1,77 @@
+import gulp from 'gulp';
+import path from 'path';
+import chalk from 'chalk';
+import childProcess from 'child_process';
+import { build as coreBuild, sendHotReloadChangeNotification, getEnvironmentDistDirectory } from './index';
+
+/**
+ * 
+ */
+function _getBuildDirectory() {
+    return path.resolve(__dirname);
+}
+
+/**
+ * 
+ */
+function _build(opts) {
+    let fn = function() {
+        return new Promise((resolve, reject) => {
+            const cmd = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
+            const env = opts.env.toLowerCase();
+            
+            const args = [
+                'run',
+                'ng',
+                'build',
+                '--'
+            ];
+    
+            if (opts.watch) {
+                args.push('--watch');
+            } else {
+                if (env == 'prod' || env == 'production' || env == 'release') {
+                    args.push('--prod');
+                } else {
+                    args.push('--aot');
+                }
+            }
+    
+            args.push('--app=' + opts.app);
+            args.push('--deploy-url=' + opts.app + '');
+            args.push('--output-path=' + path.join(getEnvironmentDistDirectory(opts.env), opts.app));
+    
+            const handle = childProcess.spawn(cmd, args);
+            handle.stdout.pipe(process.stdout);
+            handle.stderr.pipe(process.stderr);
+    
+            if (opts.watch) {
+                handle.stdout.on('data', data => {
+                    let dataStr = data.toString();
+                    if (dataStr.indexOf("Hash: ") > -1) {
+                        console.log(chalk.underline.bold.bgRed.yellow('The angular app "' + opts.app + '" has been reloaded'));
+                        sendHotReloadChangeNotification();
+                    }
+                });
+            }
+
+            handle.on('close', (code, signal) => {
+                resolve();
+            });
+
+            handle.on('error', (err) => {
+                reject(err);
+            });
+        });
+    }
+    fn.displayName = 'Angular Build for app "' + opts.app + '"';
+    return fn;
+}
+
+export function build(opts) {
+    return gulp.parallel(
+        coreBuild(opts), 
+        _build(Object.assign({}, opts, {app: 'action'})), 
+        _build(Object.assign({}, opts, {app: 'options'}))
+    );
+}
